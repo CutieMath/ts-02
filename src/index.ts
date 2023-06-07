e66b87b7a720a0e7af63e34ad3fadd92f80fa69a
Yuxin Ye <yuxin_ye@protonmail.com>
2023-06-07 16:24:58 +1000

    getters and setters

--- src/index.ts
+++ src/index.ts
@@ -16,10 +16,34 @@
 // Short way
 // ==============
 class User {
+    private _courseCount = 1;
     private readonly city: string = "Melbourne" // only accessible within the class
     constructor(
         public email: string, public name: string
     ){}
+
+    // private methods, only accessible within the class
+    private deleteToken(){
+        console.log("Token deleted");
+    }
+
+    // variable edits
+    get getAppleEmail(): string {
+        return `apple${this.email}`
+    }
+
+    // Expose private variables
+    get courseCount(): number {
+        return this._courseCount;
+    }
+
+    // no return types for setters
+    set courseCount(count: number) {
+        if(count <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = count;
+    }
 }
 
 
