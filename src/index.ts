d391b28054b0bf75cc3957a93234a97f1e738a6f
Yuxin Ye <yuxin_ye@protonmail.com>
2023-06-07 14:44:44 +1000

    different ways in writing typeScript

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
+// ==============
+// Verbose way
+// ==============
+// class User {
+//     public email: string; // public is the default if not declared
+//     public name: string;
+//     private readonly city: string = "Melbourne" // only accessible within the class
+//     constructor(email: string, name: string) {
+//         this.email = email;
+//         this.name = name;
+//     }
+// }
+
+
+// ==============
+// Short way
+// ==============
 class User {
-    email: string;
-    name: string;
-    readonly city: string = "Melbourne"
-    constructor(email: string, name: string) {
-        this.email = email;
-        this.name = name;
-    }
+    private readonly city: string = "Melbourne" // only accessible within the class
+    constructor(
+        public email: string, public name: string
+    ){}
 }
+
 
 // Create the object
 const cutie = new User( "123@email.com","Cutie");
