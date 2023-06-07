76324eedd43ee80490e98c3d96f4035fc62e8f82
Yuxin Ye <yuxin_ye@protonmail.com>
2023-06-07 14:25:46 +1000

    create a constructor for ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Here");
+class User {
+    email: string;
+    name: string;
+    constructor(email: string, name: string) {
+        this.email = email;
+        this.name = name;
+    }
+}
