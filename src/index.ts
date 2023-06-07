53e917b91677a8b8367d3b26f5dad6ffaac40abd
Yuxin Ye <yuxin_ye@protonmail.com>
2023-06-07 14:32:32 +1000

    create object from constructor

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 class User {
     email: string;
     name: string;
+    readonly city: string = "Melbourne"
     constructor(email: string, name: string) {
         this.email = email;
         this.name = name;
     }
 }
+
+// Create the object
+const cutie = new User( "123@email.com","Cutie");
